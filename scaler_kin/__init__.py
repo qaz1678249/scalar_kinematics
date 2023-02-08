658ba8a8ec405bdeb114249434f3e9f70480ccc3
Feng Xu <xufengmax@g.ucla.edu>
2023-02-07 20:49:15 -0800

    added Leg import in init

--- scaler_kin/__init__.py
+++ scaler_kin/__init__.py
@@ -1,3 +1,4 @@
 from .hardware_constants import SiLVIA_consts, SCALER_walking_consts, SCALER_climbing_consts_6DoF, SCALER_climbing_consts_6DoF_gripper, SCALER_climbing_consts_4DoF
 from .SCALAR_kinematics import scalar_k
+from .SCALER_v2_Leg_6DOF_gripper import Leg
 from .utils_SCALER import ScalerStandardUtilMethods, SCALERv1UtilMethods, SCALERv2UtilMethods
