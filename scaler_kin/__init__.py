98e33899d902a356a9d4d8e66dd3e90b9681ec85
Feng Xu <280338134@qq.com>
2023-02-09 18:29:59 -0800

    update hardware constants

--- scaler_kin/__init__.py
+++ scaler_kin/__init__.py
@@ -1,5 +1,5 @@
-from .v2.hardware_constants import SiLVIA_consts, SCALER_walking_consts, SCALER_climbing_consts_6DoF, SCALER_climbing_consts_4DoF
-from .v3.hardware_constants import SCALER_climbing_consts_6DoF_gripper
+from .v2.hardware_constants import SiLVIA_consts, SCALER_walking_consts, SCALER_climbing_consts_4DoF
+from .v3.hardware_constants import SCALER_climbing_consts_6DoF, SCALER_climbing_consts_6DoF_gripper
 from .v3.SCALAR_kinematics import scalar_k
 from .v3.SCALER_v2_Leg_6DOF_gripper import Leg
 from .utils_SCALER import ScalerStandardUtilMethods, SCALERv1UtilMethods, SCALERv2UtilMethods
--- scaler_kin/v3/__init__.py
+++ scaler_kin/v3/__init__.py
@@ -1,3 +1,3 @@
-from .hardware_constants import SCALER_climbing_consts_6DoF_gripper
+from .hardware_constants import SCALER_climbing_consts_6DoF, SCALER_climbing_consts_6DoF_gripper
 from .SCALAR_kinematics import scalar_k
 from .SCALER_v2_Leg_6DOF_gripper import Leg
